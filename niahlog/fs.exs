95b140c97f7df727150ff3d2376c40b199d58b1a
niahoo <dev@ooha.in>
2016-03-01 01:08:28 +0100

    3 level typography !

--- niahlog/fs.exs
+++ niahlog/fs.exs
@@ -23,10 +23,10 @@ IO.puts """
 For a line height of #{size}px :
 
 rows        #{Helper.pad(1)} #{Helper.pad(2)} #{Helper.pad(3)} #{Helper.pad(4)} #{Helper.pad(5)}
-line-height #{Helper.pad(1 * size)} #{Helper.pad(2 * size)} #{Helper.pad(3 * size)} #{Helper.pad(4 * size)} #{Helper.pad(5 * size)}
+height      #{Helper.pad(1 * size)} #{Helper.pad(2 * size)} #{Helper.pad(3 * size)} #{Helper.pad(4 * size)} #{Helper.pad(5 * size)}
 
-rows        #{Helper.pad(1.5)} #{Helper.pad(2.5)} #{Helper.pad(3.5)} #{Helper.pad(4.5)} #{Helper.pad(5.5)}
-line-height #{Helper.pad(1.5 * size)} #{Helper.pad(2.5 * size)} #{Helper.pad(3.5 * size)} #{Helper.pad(4.5 * size)} #{Helper.pad(5.5 * size)}
+rows        #{Helper.pad(0.5)} #{Helper.pad(1.5)} #{Helper.pad(2.5)} #{Helper.pad(3.5)} #{Helper.pad(4.5)} #{Helper.pad(5.5)}
+height      #{Helper.pad(0.5 * size)} #{Helper.pad(1.5 * size)} #{Helper.pad(2.5 * size)} #{Helper.pad(3.5 * size)} #{Helper.pad(4.5 * size)} #{Helper.pad(5.5 * size)}
 """
 System.halt(0)
 
