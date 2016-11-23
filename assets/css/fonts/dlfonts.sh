20f7410e734788c9be8936db61d80228869ee034
niahoo <dev@ooha.in>
2016-11-23 14:57:31 +0100

    use merriweather 400, but more spaced, and more grey

--- assets/css/fonts/dlfonts.sh
+++ assets/css/fonts/dlfonts.sh
@@ -1,16 +1,16 @@
-wget "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6nrpPmMW7T3WCII-UZW2GGCw.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6nqAAODQ6hKuKTT6yBk7tJss.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6nhLwFvNxpkIoVzlXSkPSYkA.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6nk4GofcKVZz6wtzX_QUIqsI.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6nrPaLDTSXeXFRL0dx0NiHII.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6ntkZ6ofoXFVR94fBb90XSvU.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6nuIaMZP5eRGvEWe_CNIU_oY.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6nnNuWYKPzoeKl5tYj8yhly0.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIFvxabc8-Ua_u_bDyl2V6ao.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwII_1JgVyIQZ7vlaYT5eo8zo.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIJpID8tihKRMKfpfDQ5OzSE.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIHM1hBkL4m68a-ReB_y0NYc.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIEVUTRPtBn3BSc_rW_ryx7U.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIDaggPqPUlqZtH2DnDrcMQE.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIOR3LJ09PnibSpWhv21tZ8w.woff2" --no-check-certificate
-wget "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIEcqRxak6Wc5LUVz-l064SE.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/RFda8w1V0eDZheqfcyQ4EK-j2U0lmluP9RWlSytm3ho.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/RFda8w1V0eDZheqfcyQ4EJX5f-9o1vgP2EXwfjgl7AY.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/RFda8w1V0eDZheqfcyQ4ED0LW-43aMEzIO6XUTLjad8.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/RFda8w1V0eDZheqfcyQ4EOgdm0LZdjqr5-oayXSOefg.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6nrPaLDTSXeXFRL0dx0NiHII.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6ntkZ6ofoXFVR94fBb90XSvU.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6nuIaMZP5eRGvEWe_CNIU_oY.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/ZvcMqxEwPfh2qDWBPxn6nnNuWYKPzoeKl5tYj8yhly0.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/So5lHxHT37p2SS4-t60SlJX7FfjVuZf3W7tKvrYzIk8.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/So5lHxHT37p2SS4-t60SlOb988d5RHZc66psPc8fYGQ.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/So5lHxHT37p2SS4-t60SlMAbIeegqMtwkwn2ZHUR4Nk.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/So5lHxHT37p2SS4-t60SlBMBlNeli-0RcAdOFWWLdxQ.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIEVUTRPtBn3BSc_rW_ryx7U.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIDaggPqPUlqZtH2DnDrcMQE.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIOR3LJ09PnibSpWhv21tZ8w.woff2" --no-check-certificate
+wget -nc "https://fonts.gstatic.com/s/merriweather/v13/EYh7Vl4ywhowqULgRdYwIEcqRxak6Wc5LUVz-l064SE.woff2" --no-check-certificate
