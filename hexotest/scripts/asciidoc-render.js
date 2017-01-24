4459a64f9fc5ae0bfd27060e989d8f5f17be405a
niahoo <dev@ooha.in>
2017-01-24 19:25:40 +0100

    custom asciidoc renderer, highlight js, cactus dark is now light

---
+++ hexotest/scripts/asciidoc-render.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var entities = require('entities');
+var util = require('hexo-util');
+var cheerio = require('cheerio');
+var asciidoctor = require('asciidoctor.js')();
+var processor = asciidoctor.Asciidoctor(true);
+
+var options = hexo.config.highlight;
+
+var cheerio_load_option = {
+  decodeEntities: false
+};
+
+function renderer(data, locals) {
+  var html = processor.$convert(data.text, null);
+  var $ = cheerio.load(html, cheerio_load_option);
+
+  $('.highlight code').each(function(index, elem) {
+    options.lang = elem.attribs['data-lang'];
+    var code = entities.decodeXML($(elem).text());
+    var content = util.highlight(code, options);
+    $(elem).html(content);
+  });
+
+  return $.html()
+             .replace(/{/g, '&#123;')
+             .replace(/}/g, '&#125;');
+}
+
+hexo.extend.renderer.register('ad', 'html', renderer, true);
+hexo.extend.renderer.register('adoc', 'html', renderer, true);
+hexo.extend.renderer.register('asciidoc', 'html', renderer, true);
