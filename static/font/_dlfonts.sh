735964bff6666ed2ace9df453e5b09c92e7f337f
niahoo <dev@ooha.in>
2017-02-01 14:30:36 +0100

    added local fonts & asjs helper for async custom scripts for each post

---
+++ static/font/_dlfonts.sh
@@ -0,0 +1,6 @@
+wget -nc "http://fonts.gstatic.com/s/domine/v4/4wfhKe93QxmsFlDQSCQ5wg.ttf" --no-check-certificate
+wget -nc "http://fonts.gstatic.com/s/domine/v4/BmiPqZorq0QiT-p6kf1paaCWcynf_cDxXwCLxiixG1c.ttf" --no-check-certificate
+wget -nc "http://fonts.gstatic.com/s/arsenal/v1/GvA15TjFJjIZ9c33KSPTzQ.ttf" --no-check-certificate
+wget -nc "http://fonts.gstatic.com/s/arsenal/v1/5vJ32Pe8BCA1NkX59B6Ksy3USBnSvpkopQaUR-2r7iU.ttf" --no-check-certificate
+wget -nc "http://fonts.gstatic.com/s/arsenal/v1/SyXrdER51c-tA-DaBD7hCqCWcynf_cDxXwCLxiixG1c.ttf" --no-check-certificate
+wget -nc "http://fonts.gstatic.com/s/arsenal/v1/AnUIg26c0nuMZMpNWtsDF4nF5uFdDttMLvmWuJdhhgs.ttf" --no-check-certificate
