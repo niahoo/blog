c266504c30b2c49fcb21ac8e506cd93a70620768
niahoo <dev@ooha.in>
2017-02-06 18:05:59 +0100

    can include TOCs !

--- scripts/asciidoc-render.js
+++ scripts/asciidoc-render.js
@@ -1,33 +1,41 @@
 'use strict'
 
-var entities = require('entities');
-var util = require('hexo-util');
-var cheerio = require('cheerio');
-var asciidoctor = require('asciidoctor.js')();
-var processor = asciidoctor.Asciidoctor(true);
+var path = require('path')
+var entities = require('entities')
+var util = require('hexo-util')
+var cheerio = require('cheerio')
+var asciidoctor = require('asciidoctor.js')()
+// var processor = asciidoctor.Asciidoctor(true)
+var processor = asciidoctor
+// var options = null
+var sourceDir = path.resolve(__dirname, '../source')
+var options = Opal.hash2(['safe'], {
+  safe: 0,
+  // base_dir: sourceDir // does not work :(
+})
 
-var options = hexo.config.highlight;
+var highlightOpts = hexo.config.highlight
 
 var cheerio_load_option = {
   decodeEntities: false
-};
+}
 
 function renderer(data, locals) {
-  var html = processor.$convert(data.text, null);
-  var $ = cheerio.load(html, cheerio_load_option);
+  var html = processor.convert(data.text, options)
+  var $ = cheerio.load(html, cheerio_load_option)
 
   $('.highlight code').each(function(index, elem) {
-    options.lang = elem.attribs['data-lang'];
-    var code = entities.decodeXML($(elem).text());
-    var content = util.highlight(code, options);
-    $(elem).html(content);
-  });
+    highlightOpts.lang = elem.attribs['data-lang']
+    var code = entities.decodeXML($(elem).text())
+    var content = util.highlight(code, highlightOpts)
+    $(elem).html(content)
+  })
 
   return $.html()
     // .replace(/{/g, '&#123;')
-    // .replace(/}/g, '&#125;');
+    // .replace(/}/g, '&#125;')
 }
 
-hexo.extend.renderer.register('ad', 'html', renderer, true);
-hexo.extend.renderer.register('adoc', 'html', renderer, true);
-hexo.extend.renderer.register('asciidoc', 'html', renderer, true);
+hexo.extend.renderer.register('ad', 'html', renderer, true)
+hexo.extend.renderer.register('adoc', 'html', renderer, true)
+hexo.extend.renderer.register('asciidoc', 'html', renderer, true)
