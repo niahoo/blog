e4de2a2cc41a56530c57f188ac122a8276e1a690
niahoo <dev@ooha.in>
2017-08-14 14:39:10 +0200

    deploying jekyll with node scp

--- run-deploy.js
+++ run-deploy.js
@@ -3,24 +3,35 @@
 var dotenv = require('dotenv')
 var prompt = require('prompt')
 var scp2 = require('scp2')
+var colors = require("colors/safe")
 
 dotenv.load()
 prompt.start()
 
+
+prompt.message = colors.green('scp')
+prompt.delimiter = colors.green(' <- ')
+
 var buildDir = process.env.BUILD_PROD_DIR + '/'
 var remoteDir = process.env.REMOTE_PROD_DIR + '/'
 var remoteHost = process.env.DEPLOY_HOST
+
+console.log("Enter username and password")
+
 var promptSchema = {
     properties: {
         username: {
-            required: true
+            required: true,
+            name: "Host user name",
         },
         password: {
             required: true,
             hidden: true,
+            name: "Password",
         },
     }
 }
+
 prompt.get(promptSchema, function(err, result){
     console.log("Local dir   %s", buildDir)
     console.log("Remote dir  %s", remoteDir)
